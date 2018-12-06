6b0f3ecd1e926fc1b4f7312f6c4c186136dda56a
ryanmcvicker12 <36174949+ryanmcvicker12@users.noreply.github.com>
2018-12-06 12:08:38 -0500

    Update testsearch.py

--- testsearch.py
+++ testsearch.py
@@ -6,6 +6,7 @@ import os
 
 
 def main():
+    #method that searches for a question
     question = "http error 503?"
     searchq = search(question, num=2)
     list_of_urls = [url for url in searchq]
@@ -17,15 +18,29 @@ def main():
         print("________________________________________________________________")
         
         
-def sub():
-    question = "how to tie a tie?"
+def sub(question):
+    
+    #search the question on google
     searchq = search(question, num=2)
+    #create a list of urls returned from searchq
     list_of_urls = [url for url in searchq]
+    #index the first url due to timeout restraint
     url = list_of_urls[0]
+    #parse through soup([]) for the elements returned 
+    for script in soup(["script","style"]):
+        #delete the elements returned 
+        script.decompose() 
+     #get all text from the modified soup
+    text = soup.get_text() 
+    #what does this do????
+    lines = (line.strip() for line in text.splitlines()
+    #what does this do???
+    chunks = (phrase.strip() for line in lines for phase in line.split("  "))
+    print(text.encode('utf-8'))
     
-    for x in 
-    
-
-
+    try:
+             #check if file exists 
+             
+ 
 if __name__ == '__main__':
     main()
