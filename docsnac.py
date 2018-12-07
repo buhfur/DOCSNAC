68c81ef1c99f09f01220f6026577929cac6eb244
ryanmcvicker12 <ryanmcvicker12@gmail.com>
2018-12-06 20:39:33 -0500

    changed structure of classes in order to make everything more readable

--- docsnac.py
+++ docsnac.py
@@ -67,7 +67,7 @@ ________________________________________________________________________________
 
 
 """
-#this is on child branch
+
 
 import time
 import os
@@ -77,24 +77,22 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 #TODO: need to add more exceptions so testers can tell me what happened
 
-class TextFile:
-    """
-    return : array of questions parsed from file
-    """
 
+class SearchQuestions:
 
-    def __init__(self, dir):
+    def write_to_file(self):
+        #algorithm for fake-typing, creating the new file to be
+        return
+        
+    """
+    return : <returns list of questions parsed from file
+    """
+    def parse_file(self, dir):
+
         self.filename = dir.split('\t')[-1]
-        #call parse file here
 
-        #pass it on to searchquestions.Searchquestions?
-
-    def get_file_name(self):
-        return self.filename
-
-    def parse_file(self):
         #parse the file and look for questions
-        if not self.filename.endswith('.txt'): #check to make sure file is text
+        if not self.filename.endswith('.txt'): #check to make sure file is text file
             return "Error: file needs to be text file"
             #ADD EXCEPTION HERE!!
         else:
@@ -105,21 +103,21 @@ class TextFile:
                 print("parsing %s for questions...." % self.filename)
 
                 for line_in_file in file_data.split('\n'):
-                    if line_infile.endswith("?"):
+                    if line_in_file.endswith("?"):
                         list_of_questions.append(line_in_file)
+                self.search_questions(list_of_questions)
 
                 return list_of_questions
 
 
-
-class SearchQuestions(TextFile):
-
-    def search_questions(self, array_of_questions, filename):
+    def search_questions(self, array_of_questions):
         #create separate file for questions and answers
+
+
 
         for question in array_of_questions:
             #search for answer here
-            found_urls = search(query_question, num=10) #get 10 results
+            found_urls = search(question, num=10) #get 10 results
             #make get request to web page
             list_of_urls = [url for url in found_urls]
 
@@ -133,16 +131,17 @@ class SearchQuestions(TextFile):
                 for script in _soup(["script", "style"]):
                     script.decompose()
 
-                _text = soup.get_text()
+                 _text = soup.get_text()
 
-                lines = [line.strip() for line in _text.splitlines()]
+                 lines = [line.strip() for line in _text.splitlines()]
 
-                chunk = [phrase.strip() for line in lines for phrase in line.split(' ')]
-                _text = '\n'.join(chunk for chunk in chunks if chunk)
+                 chunk = [phrase.strip() for line in lines for phrase in line.split(' ')]
+                 _text = '\n'.join(chunk for chunk in chunks if chunk)
 
                  #now write the question text to new  file unless stated other wise
-                 #write to a new file
                  self.write_to_file()
+
+
 
 
 
@@ -153,19 +152,12 @@ class SearchQuestions(TextFile):
 
 if __name__ == '__main__':
 
-    snac_list = [x for x in second_text.split('\n')]
-    for new in snac_list:
-        time.sleep(0.3)
-        print(new)
+    print(second_text)
     while True:
         question_file = input("""
         =====================
         | ENTER DIR NAME HERE | : """)
-        if question_file == "quit":
+        if user_input == "quit":
             break
 
-
-
-        snac = TextFile(question_file)
-        returned_list_of_questions = snac.parse_file()
-        SearchQuestions.search_questions(returned_list_of_questions)
+        SearchQuestions.parse_file(question_file)
