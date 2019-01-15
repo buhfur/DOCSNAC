ddea029950a313d863e21f7794044ec1e03c576c
ryanmcvicker12 <ryanmcvicker12@gmail.com>
2019-01-15 17:10:22 +0000

    added two more scripts

--- docsnac/docsnac.py
+++ docsnac/docsnac.py
@@ -4,44 +4,6 @@
 #command line interface that automatically finds answers to parsed questions in document
 # ex: python bookworm.py <filename>
 
-second_text = """
-v1.1--
-
-▓█████▄  ▒█████   ▄████▄    ██████  ███▄    █  ▄▄▄       ▄████▄
-▒██▀ ██▌▒██▒  ██▒▒██▀ ▀█  ▒██    ▒  ██ ▀█   █ ▒████▄    ▒██▀ ▀█
-░██   █▌▒██░  ██▒▒▓█    ▄ ░ ▓██▄   ▓██  ▀█ ██▒▒██  ▀█▄  ▒▓█    ▄
-░▓█▄   ▌▒██   ██░▒▓▓▄ ▄██▒  ▒   ██▒▓██▒  ▐▌██▒░██▄▄▄▄██ ▒▓▓▄ ▄██▒
-░▒████▓ ░ ████▓▒░▒ ▓███▀ ░▒██████▒▒▒██░   ▓██░ ▓█   ▓██▒▒ ▓███▀ ░
- ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ░▒ ▒  ░▒ ▒▓▒ ▒ ░░ ▒░   ▒ ▒  ▒▒   ▓▒█░░ ░▒ ▒  ░
- ░ ▒  ▒   ░ ▒ ▒░   ░  ▒   ░ ░▒  ░ ░░ ░░   ░ ▒░  ▒   ▒▒ ░  ░  ▒
- ░ ░  ░ ░ ░ ░ ▒  ░        ░  ░  ░     ░   ░ ░   ░   ▒   ░
-   ░        ░ ░  ░ ░            ░           ░       ░  ░░ ░
- ░               ░                                      ░
-
-________________________________________________________________________________
-This is docsnac your tool for auto completing your
-assignments
-________________________________________________________________________________
-Made by : Ryan McVicker
-________________________________________________________________________________
-Date : 12/3/2018
-________________________________________________________________________________
-Please use this software as a means of auto-completing your assignments |
-________________________________________________________________________________
-TYPE QUIT TO END APPLICATION
-
--Please enter the directory of your text file that has all the questions.
- _________________________________________
-|       WHAT DOES THIS PROGRAM DO????    |
------------------------------------------
-|   1. finds the predicted answer to your questions on a text file
-|   2. uses an algorithm to enter the characters as if you are the one who typed it
-|   3. saves the anwers to a text document which you can then upload to google classroom
-________________________________________________________________________________
-
-
-
-"""
 import random
 import sys
 import time
@@ -94,7 +56,7 @@ def write_to_file(text, filename=None):
 
 
 
-
+#what is this method used for ?
 def parse_directory_file(directory):
     #split list to get filename at end of dir string
     filename = directory.split('\t')[-1]
@@ -115,7 +77,6 @@ def parse_directory_file(directory):
             return list_of_questions
 
 
-    """ <return> : returns a dictionary of [question:answer] form """
 
 def search_questions(array_of_questions):
     q_and_a = {}
@@ -124,17 +85,20 @@ def search_questions(array_of_questions):
         list_of_urls = [url for url in search(question, num=10)]
 
 
-
         try:
 
             _url = random.choice(list_of_urls)
+
             _soup = BeautifulSoup(urlopen(_url))
+
             IS_VALID_URL = True
+
             for script in _soup(["script", "style"]):
+
                 script.decompose()
 
-
             #get text from soup
+
             _text = _soup.get_text()
             shorten_text(_text, char=80) #PROPOSED METHOD
 
@@ -146,46 +110,3 @@ def search_questions(array_of_questions):
 
 
 
-
-
-
-
-
-
-
-if __name__ == '__main__':
-
-    print(second_text)
-    #init() filters out any ANSI codes, thus starting colorama
-    init()
-    print(Fore.RED + ' \t\tWARNING:\n \ti am not responsible for any misuse of this program be it through\n \tplagiarism or any other abuse')
-    #restore stdin to its normal state, diabling colorama
-    print(Style.RESET_ALL)
-    while True:
-
-        question_file = input("""
-        \n\n
-        _______________________
-        | ENTER DIR NAME HERE | :  """)
-
-        if question_file == "quit":
-            #end the program
-            sys.exit()
-
-        while not os.path.exists(question_file):
-
-            question_file = input("""
-            \n\n
-            _______________________
-            | ENTER DIR NAME HERE | :  """)
-
-
-        try:
-
-
-            array_of_questions = parse_directory_file(question_file)
-            question_dict = search_questions(array_of_questions)
-            write_to_file(question_dict, filename=None)
-
-        except Exception as e:
-            print(e)
---
+++ docsnac/startup.py
@@ -0,0 +1,52 @@
+#initializer for the entire program
+
+
+second_text = """
+v1.1--
+
+▓█████▄  ▒█████   ▄████▄    ██████  ███▄    █  ▄▄▄       ▄████▄
+▒██▀ ██▌▒██▒  ██▒▒██▀ ▀█  ▒██    ▒  ██ ▀█   █ ▒████▄    ▒██▀ ▀█
+░██   █▌▒██░  ██▒▒▓█    ▄ ░ ▓██▄   ▓██  ▀█ ██▒▒██  ▀█▄  ▒▓█    ▄
+░▓█▄   ▌▒██   ██░▒▓▓▄ ▄██▒  ▒   ██▒▓██▒  ▐▌██▒░██▄▄▄▄██ ▒▓▓▄ ▄██▒
+░▒████▓ ░ ████▓▒░▒ ▓███▀ ░▒██████▒▒▒██░   ▓██░ ▓█   ▓██▒▒ ▓███▀ ░
+ ▒▒▓  ▒ ░ ▒░▒░▒░ ░ ░▒ ▒  ░▒ ▒▓▒ ▒ ░░ ▒░   ▒ ▒  ▒▒   ▓▒█░░ ░▒ ▒  ░
+ ░ ▒  ▒   ░ ▒ ▒░   ░  ▒   ░ ░▒  ░ ░░ ░░   ░ ▒░  ▒   ▒▒ ░  ░  ▒
+ ░ ░  ░ ░ ░ ░ ▒  ░        ░  ░  ░     ░   ░ ░   ░   ▒   ░
+   ░        ░ ░  ░ ░            ░           ░       ░  ░░ ░
+ ░               ░                                      ░
+
+________________________________________________________________________________
+This is docsnac your tool for auto completing your
+assignments
+________________________________________________________________________________
+Made by : Ryan McVicker
+________________________________________________________________________________
+Date : 12/3/2018
+________________________________________________________________________________
+Please use this software as a means of auto-completing your assignments |
+________________________________________________________________________________
+TYPE QUIT TO END APPLICATION
+
+-Please enter the directory of your text file that has all the questions.
+ _________________________________________
+|       WHAT DOES THIS PROGRAM DO????    |
+-----------------------------------------
+|   1. finds the predicted answer to your questions on a text file
+|   2. uses an algorithm to enter the characters as if you are the one who typed it
+|   3. saves the anwers to a text document which you can then upload to google classroom
+________________________________________________________________________________
+
+list of commands:
+
+
+"""
+
+
+
+
+
+if __main__ == '__name__':
+
+    while True:
+
+        user_command = input().split()
